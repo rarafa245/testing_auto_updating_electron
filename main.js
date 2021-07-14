99321099fbbfc65d0ef26728669aad978c019494
programadorLhama <programadorlhama@gmail.com>
2021-07-13 23:43:13 -0300

    feat: Implementing auto-update

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
+const { autoUpdater } = require('electron-updater')
 
 let mainWindow;
 
@@ -16,6 +17,11 @@ function createWindow () {
   mainWindow.webContents.openDevTools()
   mainWindow.on('closed', function () {
     mainWindow = null;
+  });
+
+  // adding callback to ready
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
   });
 }
 
@@ -37,4 +43,17 @@ app.on('activate', function () {
 
 ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
+});
+
+// Adding this callbacks
+autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+    mainWindow.webContents.send('update_downloaded');
+});
+
+ipcMain.on('restart_app', () => {
+    autoUpdater.quitAndInstall();
 });
