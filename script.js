9eda5e5efa87c02fa9e524f3a55c9be7830b8c64
programadorLhama <programadorlhama@gmail.com>
2021-07-13 23:21:51 -0300

    feat: implement first electron app

---
+++ script.js
@@ -0,0 +1,9 @@
+const {ipcRenderer} = require('electron')
+const version = document.getElementById('version')
+
+ipcRenderer.send('app_version')
+ipcRenderer.on('app_version', (event, arg) => {
+    console.log(arg)
+    ipcRenderer.removeAllListeners('app_version')
+    version.innerText = 'Version ' + arg.version
+})
